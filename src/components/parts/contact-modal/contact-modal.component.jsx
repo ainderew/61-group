c9fbcefb89b3e07fbd6c3f6e9ef9e1ef5748dc1c
Andrew <andrewapinon@gmail.com>
2020-07-06 15:49:32 +0800

    added contact form functionality

--- src/components/parts/contact-modal/contact-modal.component.jsx
+++ src/components/parts/contact-modal/contact-modal.component.jsx
@@ -8,6 +8,35 @@ export const ContactModal = ({modalState,parentFunction}) =>{
     //USESTATE VARS
     const [modalClass, setModalClass] = useState("contact-modal")
     
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: ""
+    })
+    
+    //FUNCTIONS
+    const formInputChange = (key,e) =>{
+        setFormData({
+            ...formData,
+            [{key}]: e.target.value
+        })
+    }
+    
+    const sendForm = async (e) =>{
+        e.preventDefault();
+        
+        await fetch("http://localhost:5000/email", {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        })
+        .then ( response => response.json())
+        .then ( data => console.log(data))
+    }
+    
     //USEEFFECT
     useEffect(() => {
         if(modalState){
@@ -25,20 +54,20 @@ export const ContactModal = ({modalState,parentFunction}) =>{
             <div className="contact-form-row">
                 <div className="contact-form-input-container">
                     <label htmlFor="name" className="contact-form-label">Name</label>
-                    <input type="text" name="name" placeholder="Enter Your Name" className="contact-form-input input-name"/>
+                    <input value={formData.name} onChange={(e) => formInputChange("name",e)} type="text" name="name" placeholder="Enter Your Name" className="contact-form-input input-name"/>
                 </div>
                 <div className="contact-form-input-container">
                     <label htmlFor="email" className="contact-form-label">Email/Phone</label>
-                    <input type="text" name="email" placeholder="Enter Your Email and/or Phone Number" className="contact-form-input"/>
+                    <input value={formData.email} onChange={(e) => formInputChange("email",e)} type="text" name="email" placeholder="Enter Your Email and/or Phone Number" className="contact-form-input"/>
                 </div>
             </div>
             <div className="contact-form-row">
                 <div className="contact-form-input-container">
                     <label htmlFor="message" className="contact-form-label">Message</label>
-                    <textarea type="textarea" name="message" placeholder="Enter Your Name" className="contact-form-textarea"/>
+                    <textarea value={formData.message} onChange={(e) => formInputChange("message",e)} type="textarea" name="message" placeholder="Enter Your Name" className="contact-form-textarea"/>
                 </div>
             </div>
-            <button className="contact-form-btn">SUBMIT</button>
+            <button onClick={sendForm} className="contact-form-btn">SUBMIT</button>
             </form>
         </div>
     )
