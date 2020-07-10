93dcd1da98d691ef19b7cbf786d846859130b72e
Andrew <andrewapinon@gmail.com>
2020-07-10 11:10:30 +0800

    switch to server side rendering

---
+++ build/precache-manifest.3f1a968ad99855eec2e91792bf278369.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18c33d59b51c746bebbe0f0879507d37",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7b0c9a55e5ab039cdc6",
+    "url": "/static/css/main.c8221aaa.chunk.css"
+  },
+  {
+    "revision": "d633c3394f97ae09d772",
+    "url": "/static/js/2.44f5b606.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/static/js/2.44f5b606.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b0c9a55e5ab039cdc6",
+    "url": "/static/js/main.fcaac066.chunk.js"
+  },
+  {
+    "revision": "c9fa7f1c96df3475a8bf",
+    "url": "/static/js/runtime-main.52429160.js"
+  },
+  {
+    "revision": "bd6f54ea89009171bfdba2cdc7499a64",
+    "url": "/static/media/Logo.bd6f54ea.svg"
+  },
+  {
+    "revision": "775a6c85ebf33f7fdf007d2b469c9dd1",
+    "url": "/static/media/Silica sand fines.775a6c85.webp"
+  },
+  {
+    "revision": "dc4d4fde8615b40b978683e892e79fb3",
+    "url": "/static/media/Silica sand fines.dc4d4fde.jpg"
+  },
+  {
+    "revision": "5a328357e85b10bf6fd9fe1d65028c04",
+    "url": "/static/media/Silica sand of large fractions.5a328357.jpg"
+  },
+  {
+    "revision": "c6eae9e0165aad1eb5a08f0419e4fbd6",
+    "url": "/static/media/Silica sand of large fractions.c6eae9e0.webp"
+  },
+  {
+    "revision": "1d7e0d733d2ca0be9c09ab70e619345a",
+    "url": "/static/media/Silica sand of medium fractions.1d7e0d73.jpg"
+  },
+  {
+    "revision": "275313344b48536fe98ee1e31558351c",
+    "url": "/static/media/Silica sand of medium fractions.27531334.webp"
+  },
+  {
+    "revision": "6eabb80ffe4c956c6cea4bf07be1a355",
+    "url": "/static/media/White fraction quartz.6eabb80f.jpg"
+  },
+  {
+    "revision": "cb94464a0e1a4e7ed35da3a5ea59ac23",
+    "url": "/static/media/White fraction quartz.cb94464a.webp"
+  },
+  {
+    "revision": "e9d553dac5ea0754dc3bf180a96aaa1b",
+    "url": "/static/media/White_quartz_productpage.e9d553da.webp"
+  },
+  {
+    "revision": "56ecaf1c0d885431a8372b64369945b4",
+    "url": "/static/media/about-us-1.56ecaf1c.webp"
+  },
+  {
+    "revision": "bf9e3f8f13188dccb1b7fbe6caa48442",
+    "url": "/static/media/about-us-1.bf9e3f8f.jpg"
+  },
+  {
+    "revision": "025f47d940c391276bf139863a33f3d0",
+    "url": "/static/media/about-us-2.025f47d9.webp"
+  },
+  {
+    "revision": "d3315cb96cfe5de1d055c8ee34e9e970",
+    "url": "/static/media/about-us-2.d3315cb9.jpg"
+  },
+  {
+    "revision": "522842d91b530fc3cc59b554ff822cad",
+    "url": "/static/media/close.522842d9.svg"
+  },
+  {
+    "revision": "83d8649fa26e8b43d4a9e57ad931d23c",
+    "url": "/static/media/contact-photo.83d8649f.webp"
+  },
+  {
+    "revision": "bed5ccec0ce9d6ab43ee5191e85e74dd",
+    "url": "/static/media/contact-photo.bed5ccec.jpg"
+  },
+  {
+    "revision": "7d98a903ce076b7a9d38382f11ae06ac",
+    "url": "/static/media/delivery.7d98a903.svg"
+  },
+  {
+    "revision": "a6cfa84b3baa5ccc33782c9c6ed6c78a",
+    "url": "/static/media/footer-down.a6cfa84b.svg"
+  },
+  {
+    "revision": "269d55de85e51980ef20b9e1b2e9df0e",
+    "url": "/static/media/footer-image-1080.269d55de.webp"
+  },
+  {
+    "revision": "bf47d7a53a520b5959ff36773db9c045",
+    "url": "/static/media/footer-image.bf47d7a5.webp"
+  },
+  {
+    "revision": "6f2e24dc33e69e949bb7051bf137291f",
+    "url": "/static/media/homepage-background-phone.6f2e24dc.jpg"
+  },
+  {
+    "revision": "f24b180f3980b2d5a537e43a9f3baf5e",
+    "url": "/static/media/homepage-background.f24b180f.jpg"
+  },
+  {
+    "revision": "a9fba4f4d383ce872ce90d87a85ea730",
+    "url": "/static/media/hp-row-4-img.a9fba4f4.webp"
+  },
+  {
+    "revision": "1b3ef79d85b500ae8ef5bda7c1275b80",
+    "url": "/static/media/location.1b3ef79d.svg"
+  },
+  {
+    "revision": "51c44790b31f99759ffa25c30cca8687",
+    "url": "/static/media/mail_white.51c44790.svg"
+  },
+  {
+    "revision": "5ec365d594a37a7b51df62d49fe8c09a",
+    "url": "/static/media/menu2.5ec365d5.svg"
+  },
+  {
+    "revision": "550a7bb101531a5745f3da4374aa7851",
+    "url": "/static/media/news1.550a7bb1.jpg"
+  },
+  {
+    "revision": "edd59ec09fd6667883c5abf4494a4217",
+    "url": "/static/media/news1.edd59ec0.webp"
+  },
+  {
+    "revision": "412c40fb05ccf3cf5c36ebb1007b2380",
+    "url": "/static/media/phone.412c40fb.svg"
+  },
+  {
+    "revision": "66b3c957aadc4e8dfe5c2e592e093dc4",
+    "url": "/static/media/phone_white.66b3c957.svg"
+  },
+  {
+    "revision": "0cef30b4e274e50c4935a7c8664d8673",
+    "url": "/static/media/products-header.0cef30b4.webp"
+  },
+  {
+    "revision": "892574eee3d5f422ae1f1cb439b190c5",
+    "url": "/static/media/products-header.892574ee.jpg"
+  },
+  {
+    "revision": "6e8cc30d0459394d1b354fe292913564",
+    "url": "/static/media/russia.6e8cc30d.svg"
+  },
+  {
+    "revision": "5038068f3f76e2da7a096f24a6938720",
+    "url": "/static/media/silica-sand-article-home-img.5038068f.webp"
+  },
+  {
+    "revision": "8b0e4eff7482e4d724377fb8af782703",
+    "url": "/static/media/silica-sand-article-img-1.8b0e4eff.jpg"
+  },
+  {
+    "revision": "c9d045d6172b493122f3a16680c436c9",
+    "url": "/static/media/silica-sand-article-img-1.c9d045d6.webp"
+  },
+  {
+    "revision": "0fb8091fc620e347d5b1c050ac94a063",
+    "url": "/static/media/silica-sand-article-img-2.0fb8091f.jpg"
+  },
+  {
+    "revision": "015ffdda780e5145792ff9d994bd4362",
+    "url": "/static/media/silica-sand-article-img-3.015ffdda.jpg"
+  },
+  {
+    "revision": "7666ef4422eafa7b05a880ff0be2cf06",
+    "url": "/static/media/silica-sand-article-img-3.7666ef44.webp"
+  },
+  {
+    "revision": "5974f32f04b7e7f3dee3833853bab059",
+    "url": "/static/media/silica-sand-article.5974f32f.jpg"
+  },
+  {
+    "revision": "f2375af1fc09a7db81a59945da5703b1",
+    "url": "/static/media/time.f2375af1.svg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/static/media/uk.e5564902.svg"
+  },
+  {
+    "revision": "e431dcdf395ea7405831ddeaafbc154d",
+    "url": "/static/media/white-quartz-article-hero.e431dcdf.jpg"
+  },
+  {
+    "revision": "2a67cf244481db1291f43767dbc6a5ec",
+    "url": "/static/media/white-quartz-article-img-1.2a67cf24.webp"
+  },
+  {
+    "revision": "2c8bc1a769c8a559a9da922a2a4c6904",
+    "url": "/static/media/white-quartz-article-img-1.2c8bc1a7.jpg"
+  },
+  {
+    "revision": "4914dbf73bb9017a8869067c24db5542",
+    "url": "/static/media/white-quartz-article.4914dbf7.jpg"
+  },
+  {
+    "revision": "f107b4c7678904166353eda0ac83571a",
+    "url": "/static/media/white-quartz-article.f107b4c7.webp"
+  },
+  {
+    "revision": "312df4bdf1ab1368da3fa4c189c794b3",
+    "url": "/static/media/wood-pellets-article-hero.312df4bd.jpg"
+  },
+  {
+    "revision": "0293bce5d7ace846aba0c982af94ac1a",
+    "url": "/static/media/wood-pellets-article-img-1.0293bce5.jpg"
+  },
+  {
+    "revision": "72fef05d3eee99e5aef804c319c267b2",
+    "url": "/static/media/wood-pellets-article-img-1.72fef05d.webp"
+  },
+  {
+    "revision": "20dcca605dc882340be725ba98297571",
+    "url": "/static/media/wood-pellets-article.20dcca60.jpg"
+  },
+  {
+    "revision": "695fc99c68644a50fc92b063836b5215",
+    "url": "/static/media/wood-pellets-article.695fc99c.webp"
+  },
+  {
+    "revision": "2d07be008ee64d25cf16707b6a7e01ce",
+    "url": "/static/media/wood-pellets.2d07be00.webp"
+  },
+  {
+    "revision": "fd8adcf47b15247b3f60d0fb4cbe6652",
+    "url": "/static/media/wood-pellets.fd8adcf4.jpg"
+  }
+]);
